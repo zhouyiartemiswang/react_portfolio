4129c1fb28f968743d1a80f152c05972804f2ab1
Artemis <wzyzhouyi@gmail.com>
2020-11-11 13:16:40 -0800

    added routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import NoMatch from './pages/NoMatch';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
+import { Router, Switch, Route } from "react-router-dom";
 
 function App() {
     return (
-        <div>
+        <Router>
             <NavBar />
+            <Switch>
+                <Route exact path={["/", "/about"]} component={About} />
+                <Route exact path="/portfolio" component={Portfolio} />
+                <Route exact path="/contact" component={Contact} />
+                <Route component={NoMatch} />
+            </Switch>
             <Footer />
-        </div>
+        </Router>
     );
 }
 
