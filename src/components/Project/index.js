95196f0218bec5f3c8afbb0fda6352622826d1f8
Artemis <wzyzhouyi@gmail.com>
2020-11-11 14:30:00 -0800

    added portfolio page with projects

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-export default function Project() {
+export default function Project(props) {
     return (
         <section className="row">
             <section className="col-12 col-md-6">
                 <div className="card" >
-                    <img src="../assets/img/cold_west.PNG" className="card-img-top" alt="cold_west" />
+                    <img src={props.img} className="card-img-top" alt={props.title} />
                     <div className="card-body">
-                        <h5 className="card-title">The Cold West</h5>
-                        <p className="card-text">One-stop website for winter activity lovers in Washington. Used express-handlebars and mySQL2.</p>
-                        <a href="https://the-cold-west.herokuapp.com/" className="card-link">Deployed Link</a>
-                        <a href="https://github.com/zhouyiartemiswang/Cold-West-Tours" className="card-link">GitHub Repo</a>
+                        <h5 className="card-title">{props.title}</h5>
+                        <p className="card-text">{props.description}</p>
+                        <a href={props.deployedLink} className="card-link">Deployed Link</a>
+                        <a href={props.githubRepo} className="card-link">GitHub Repo</a>
                     </div>
                 </div>
             </section>
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -4,7 +4,52 @@ import Project from '../components/Project';
 export default function Portfolio() {
     return (
         <main>
-            <Project/>
+            <Project
+                img="https://res.cloudinary.com/artemiswang/image/upload/v1605131793/cold_west_gohnyj.png"
+                title="The Cole West"
+                description="One-stop website for winter activity lovers in Washington. Used express-handlebars and mySQL2."
+                deployedLink="https://the-cold-west.herokuapp.com/"
+                githubLink="https://github.com/zhouyiartemiswang/Cold-West-Tours"
+            />
+            <Project
+                img="https://res.cloudinary.com/artemiswang/image/upload/v1605133436/user_directory_home.png"
+                title="User Directory"
+                description="A user directory to search for a specific user and sort by age."
+                deployedLink="https://react-user-directory-app.herokuapp.com/"
+                githubRepo="https://github.com/zhouyiartemiswang/react_user_directory"
+            />
+            <Project
+                img="https://res.cloudinary.com/artemiswang/image/upload/v1605131792/code_quiz_start_page_desktop_sc3uym.png"
+                title="Code Quiz"
+                description="Beginner's code quiz to test your knowledge in front-end development."
+                deployedLink="https://zhouyiartemiswang.github.io/code_quiz/"
+                githubRepo="https://github.com/zhouyiartemiswang/code_quiz"
+            />
+            <Project
+                img="https://res.cloudinary.com/artemiswang/image/upload/v1605131792/burger_restaurant_desktop_tuafv7.png"
+                title="Burger Restaurant"
+                description="A burger logger that uses Node and MySQL to query and route data in the
+                app, and Handlebars to generate HTML."
+                deployedLink="https://mighty-wave-39082.herokuapp.com/"
+                githubRepo="https://github.com/zhouyiartemiswang/node_express_handlebars_burger_restaurant"
+            />
+            <Project
+                img="https://res.cloudinary.com/artemiswang/image/upload/v1605131794/note_taker_desktop_u7bwxj.png"
+                title="Note Taker"
+                description="A handy note pad that uses an express
+                backend and save and retrieve note data from a JSON file."
+                deployedLink="https://mysqlnotetaker.herokuapp.com/"
+                githubRepo="https://github.com/zhouyiartemiswang/note_taker"
+            />
+            <Project
+                img="https://res.cloudinary.com/artemiswang/image/upload/v1605131792/weather_dashboard_desktop_glyvrs.png"
+                title="Weather Dashboard"
+                description="A weather dashboard that will run in the browser and feature dynamically
+                updated HTML and CSS."
+                deployedLink="https://zhouyiartemiswang.github.io/weather_dashboard/"
+                githubRepo="https://github.com/zhouyiartemiswang/weather_dashboard"
+            />
+
         </main>
     )
 }
